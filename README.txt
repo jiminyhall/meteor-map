2acb999a0d70d5454aeeb85c0a4abeb2b48e25bf
Jimmy Hall <jiminy.hall@gmail.com>
2017-05-14 18:23:45 +0100

    Initial commmit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/jiminyhall/pen/VmyRdp.
+
+ Using D3 to show a world map
---
+++ js/index.js
@@ -0,0 +1,139 @@
+/**
+ * Things to add
+ *   1. repeating map
+ *   2. max scroll limits
+ *   3. Sort the draw order out of the circles
+ */
+
+var width = 960,
+  height = 480;
+
+var quantile = d3.scale.quantile()
+  .range(d3.range(5).map(function(i) {
+    //console.log("Quantile " + i);
+    return 5 + (i * 2);
+  }));
+
+var classQuantile = d3.scale.quantile()
+  .range(d3.range(5).map(function(i) {
+    return "Q" + i;
+  }));
+
+var projection = d3.geo.winkel3()
+  .scale(1000)
+  .translate([width / 2, height / 2])
+  .center([0, 50]);
+
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+var path = d3.geo.path().projection(projection);
+
+var g = svg.append("g");
+
+d3.json("https://raw.githubusercontent.com/jiminyhall/maps/master/world.json", function(error, world) {
+
+  g.selectAll("path")
+    .data(topojson.feature(world, world.objects.subunits).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    /*
+    .on("mouseover", function() {
+      d3.select(this).transition().style("fill", "red");
+    })
+    .on("mouseout", function() {
+      d3.select(this).transition().duration(1000).style("fill", "black");
+    })*/;
+});
+
+// load in meteors
+d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json", function(error, meteors) {
+  var count = 1;
+  //console.log(meteors);
+
+  
+  
+  quantile.domain(meteors.features.map( function(d) {
+    
+    var val = parseInt(d.properties.mass);
+    if(val>0) {
+      //console.log("Quantile domain: " + val);
+      return val;
+    }
+  }));
+  
+  classQuantile.domain(meteors.features.map( function(d) {
+    var val = parseInt(d.properties.mass);
+    if(val>0) { return val; }
+  }));
+
+  g.selectAll("circle")
+    .data(meteors.features)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) {
+      if (d.geometry !== null)
+        return projection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[0];
+    })
+    .attr("cy", function(d) {
+      if (d.geometry !== null)
+        return projection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[1];
+    })
+    .attr("r", function(d) {
+      var r = quantile(d.properties.mass);
+      //console.log("Mass: " + d.properties.mass + " = " + r);
+      //console.log(r);
+      return r;
+    })
+    .attr("class", function(d) { return classQuantile(d.properties.mass); })
+    .on("mouseout", function(d) {
+      document.getElementById("info").style.display="none";
+    })
+    .on("mouseover", function(d) {
+      console.log(d.properties.name);
+      var str = "<h3>Info Panel</h3>" + 
+                "<strong>ID:</strong> " + d.properties.id +
+                "<br><strong>Year:</strong> " + d.properties.year.substr(0,4) +
+                "<br><strong>Name:</strong> " + d.properties.name + 
+                "<br><strong>Mass:</strong> " + d.properties.mass +
+                "<br><strong>Recclass:</strong> " + d.properties.recclass;
+    
+      document.getElementById("info").innerHTML = str;
+      document.getElementById("info").style.display="inline";
+  /*
+  
+  "mass": "1000",
+"name": "Aioun el Atrouss",
+"reclong": "-9.570280",
+"geolocation_address": null,
+"geolocation_zip": null,
+"year": "1974-01-01T00:00:00.000",
+"geolocation_state": null,
+"fall": "Fell",
+"id": "423",
+"recclass": "Diogenite-pm",
+"reclat": "16.398060",
+"geolocation_city": null,
+"nametype": "Valid"
+  
+  */
+  
+  });
+
+});
+
+// zoom and pan
+var zoom = d3.behavior.zoom()
+  .on("zoom", function() {
+    g.attr("transform", "translate(" +
+      d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")");
+    g.selectAll("g")
+      .attr("d", path.projection(projection));
+    g.selectAll("path")
+      .attr("d", path.projection(projection));
+
+  });
+
+svg.call(zoom);
